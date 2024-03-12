e0810cd007f5cc402118f4a78119c6354c12f567
Alaa Zamel <alaa.zamel23@gmail.com>
2024-03-12 09:23:06 +0300

    project initialization

---
+++ src/app.module.ts
@@ -0,0 +1,20 @@
+import { Module } from '@nestjs/common';
+import { AppConfigModule } from './config/app/config.module';
+import { DBConfigModule } from './config/database/postgresql/config.module';
+import { PostgresSQLServerDatabaseProviderModule } from './providers/database/postgresql/provider.module';
+import { LoggerConfigModule } from './config/logger/sentryio/config.module';
+import { SwaggerConfigModule } from './config/openapi/swagger/config.module';
+import { HealthController } from './api/health/health.controller';
+
+@Module({
+  imports: [
+    AppConfigModule,
+    DBConfigModule,
+    PostgresSQLServerDatabaseProviderModule,
+    LoggerConfigModule,
+    SwaggerConfigModule,
+  ],
+  controllers: [HealthController],
+  providers: [],
+})
+export class AppModule {}
---
+++ src/providers/database/postgresql/provider.module.ts
@@ -0,0 +1,28 @@
+import { DatabaseType } from 'typeorm';
+import { Module } from '@nestjs/common';
+import { TypeOrmModule, TypeOrmModuleAsyncOptions } from '@nestjs/typeorm';
+import { DBConfigModule } from '../../../config/database/postgresql/config.module';
+import { DBConfigService } from '../../../config/database/postgresql/config.service';
+
+@Module({
+  imports: [
+    TypeOrmModule.forRootAsync({
+      imports: [DBConfigModule],
+      useFactory: async (
+        dbConfigService: DBConfigService,
+        // eslint-disable-next-line @typescript-eslint/require-await
+      ) => ({
+        type: 'postgres' as DatabaseType,
+        host: dbConfigService.host,
+        port: dbConfigService.port,
+        username: dbConfigService.user,
+        password: dbConfigService.password,
+        database: dbConfigService.database,
+        autoLoadEntities: true,
+        synchronize: false,
+      }),
+      inject: [DBConfigService],
+    } as TypeOrmModuleAsyncOptions),
+  ],
+})
+export class PostgresSQLServerDatabaseProviderModule {}
