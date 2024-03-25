392e3fda013ceb43e364c5937135d32d208191f0
Alaa Zamel <alaa.zamel23@gmail.com>
2024-03-25 06:00:19 +0300

    refactor: make modifications on serializers

--- src/api/job/serializers/job.serializer.ts
+++ src/api/job/serializers/job.serializer.ts
@@ -1,14 +1,19 @@
 import { Job } from '../entities/job.entity';
 
 export class JobSerializer {
-  static serialize(job: Job): { value: number; label: string } {
+  static serialize(job?: Job): { value: number; label: string } | null {
+    if (!job) {
+      return null;
+    }
     return {
       value: job.id,
       label: job.jobName,
     };
   }
 
-  static serializeList(jobs: Job[]): { value: number; label: string }[] {
+  static serializeList(
+    jobs: Job[],
+  ): ({ value: number; label: string } | null)[] {
     return jobs.map((job) => this.serialize(job));
   }
 }
