adb4b6c84107c8dd70fcf2de0bb4b610e26e7b85
HadiBa707 <73488175+HadiBa707@users.noreply.github.com>
2024-04-21 17:41:21 +0300

    modified query forms json object shape

--- src/api/dynamic-forms/services/dynamic-forms-query.service.ts
+++ src/api/dynamic-forms/services/dynamic-forms-query.service.ts
@@ -1,19 +1,15 @@
 import { Injectable } from '@nestjs/common';
-import { DataSource, Repository } from 'typeorm';
+import { DataSource } from 'typeorm';
 import { FilledFormField } from '../entities/filled-form-field.entity';
 import { QueryFormDto } from '../dto/query-form/query-form.dto';
 import { QueryFormConditionDto } from '../dto/query-form/query-form-condition.dto';
 import { QueryOperator } from '../entities/query-operator';
-import { InjectRepository } from '@nestjs/typeorm';
 import { FilledForm } from '../entities/filled-form.entity';
+import { AttendeeEvent } from '../../attend-event/entities/attendee-event.entity';
 
 @Injectable()
 export class DynamicFormsQueryService {
-  constructor(
-    private readonly dataSource: DataSource,
-    @InjectRepository(FilledForm)
-    private readonly filledFormRepository: Repository<FilledForm>,
-  ) {}
+  constructor(private readonly dataSource: DataSource) {}
 
   async queryFilledForms(queryFormDto: QueryFormDto) {
     const query = this.dataSource
@@ -42,23 +38,75 @@ export class DynamicFormsQueryService {
 
     const result = await query.getRawMany();
 
-    const forms = await Promise.all(
-      result.map((res) => {
-        return this.filledFormRepository.find({
-          where: {
-            id: res.filled_form_id,
-            event: { id: queryFormDto.event_id },
-          },
-          relations: {
-            filledFormFields: { formField: { fieldType: true }, option: true },
-            attendee: true,
-          },
-        });
+    return await Promise.all(
+      result.map(async (res) => {
+        const filledForm = await this.dataSource
+          .getRepository(FilledForm)
+          .createQueryBuilder('filled')
+          .where('filled.id = :filledID', { filledID: res.filled_form_id })
+          .andWhere('filled.event = :eventID', {
+            eventID: queryFormDto.event_id,
+          })
+          .leftJoinAndSelect('filled.attendee', 'attendee')
+          .leftJoin('attendee.user', 'user')
+          .addSelect(['user.id', 'user.email', 'user.username'])
+          .leftJoin('filled.filledFormFields', 'field')
+          .addSelect(['field.id', 'field.value', 'field.option'])
+          .leftJoin('field.formField', 'form_field')
+          .addSelect(['form_field.id'])
+          .leftJoin('field.option', 'option')
+          .addSelect(['option.id'])
+          .leftJoinAndSelect('filled.event', 'event')
+          .leftJoinAndSelect(
+            'event.attendees',
+            'attendees',
+            'attendees.attendee = filled.attendee AND attendees.event = filled.event',
+          )
+          .getOneOrFail();
+
+        const register = await this.dataSource
+          .getRepository(AttendeeEvent)
+          .createQueryBuilder('attendee_event')
+          .where('attendee_event.attendee = :attendeeID', {
+            attendeeID: filledForm.attendee.id,
+          })
+          .andWhere('attendee_event.event = :eventID', {
+            eventID: filledForm.event.id,
+          })
+          .getOneOrFail();
+
+        return { ...this.formatObject(filledForm), status: register.status };
       }),
     );
+  }
 
-    console.log(forms);
-    return forms;
+  private formatObject(filledForm: FilledForm) {
+    return {
+      id: filledForm.id,
+      submission_date: filledForm.createdAt,
+      attendee: {
+        id: filledForm.attendee.id,
+        firstName: filledForm.attendee.firstName,
+        lastName: filledForm.attendee.lastName,
+        phoneNumber: filledForm.attendee.phoneNumber,
+        profilePictureUrl: filledForm.attendee.profilePictureUrl,
+        birthDate: filledForm.attendee.birthDate,
+        bio: filledForm.attendee.bio,
+        coverPictureUrl: filledForm.attendee.coverPictureUrl,
+        user: {
+          username: filledForm.attendee.user.username,
+          email: filledForm.attendee.user.email,
+        },
+      },
+      fields: filledForm.filledFormFields.map((field) => {
+        return {
+          id: field.id,
+          value: field.value,
+          field_id: field.formField.id,
+          option_id: field.option?.id ? field.option.id : null,
+        };
+      }),
+    };
   }
 
   private async createSubQuery(condition: QueryFormConditionDto) {
