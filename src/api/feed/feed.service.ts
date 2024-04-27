ded530c7e26695e9bbc711f55c81d53ff0ed1489
HadiBa707 <73488175+HadiBa707@users.noreply.github.com>
2024-04-27 16:58:30 +0300

    working on feed

--- src/api/feed/feed.service.ts
+++ src/api/feed/feed.service.ts
@@ -1,4 +1,47 @@
 import { Injectable } from '@nestjs/common';
+import { Event } from '../event/entities/event.entity';
+import { DataSource, Repository } from 'typeorm';
+import { GenericFilter } from '../../common/interfaces/query.interface';
+import { Organization } from '../organization/entities/organization.entity';
 
 @Injectable()
-export class FeedService {}
+export class FeedService {
+  constructor(private readonly dataSource: DataSource) {}
+
+  async getSoonEvents(query: GenericFilter) {
+    return {
+      data: await this.dataSource
+        .getRepository(Event)
+        .createQueryBuilder('e')
+        .innerJoin('e.days', 'ed')
+        .groupBy('e.id')
+        .addSelect('MIN(ed.dayDate)', 'start_day')
+        .orderBy('start_day')
+        .having('MIN(ed.dayDate) >= :nowDATE', { nowDATE: new Date() })
+        .skip((query.page - 1) * query.pageSize)
+        .take(query.pageSize)
+        .getMany(),
+      count: await this.dataSource.getRepository(Event).count(),
+    };
+  }
+
+  async getOrganizations(query: GenericFilter) {
+    return {
+      data: await this.dataSource
+        .getRepository(Organization)
+        .createQueryBuilder('org')
+        .select(['org.id', 'org.name'])
+        .addSelect('COUNT(attendee.id) AS attendees_num')
+        .addSelect('COUNT(DISTINCT event.id) AS events_num')
+        .leftJoin('org.events', 'event')
+        .leftJoin('event.attendees', 'attendee')
+        .groupBy('org.id')
+        .orderBy('events_num', 'DESC')
+        .addOrderBy('attendees_num', 'DESC')
+        .offset((query.page - 1) * query.pageSize)
+        .limit(query.pageSize)
+        .getRawMany(),
+      count: await this.dataSource.getRepository(Organization).count(),
+    };
+  }
+}
