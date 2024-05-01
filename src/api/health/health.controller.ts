f600e89cdb0c251c131b2fcf3a044cd6b3215151
HadiBa707 <73488175+HadiBa707@users.noreply.github.com>
2024-05-01 16:15:25 +0300

    refactoring code

--- src/api/health/health.controller.ts
+++ src/api/health/health.controller.ts
@@ -1,5 +1,6 @@
 import { Controller, Get } from '@nestjs/common';
 import { HealthCheck } from '@nestjs/terminus';
+import * as process from 'process';
 
 @Controller('health')
 export class HealthController {
@@ -7,5 +8,16 @@ export class HealthController {
   @HealthCheck()
   check(): string {
     return 'Service is up';
+  }
+
+  @Get('memoryUsage')
+  memoryUsage() {
+    const usage = process.memoryUsage();
+
+    return {
+      heap_total:
+        Math.round((usage.heapTotal / 1024 / 1024) * 100) / 100 + 'MB',
+      heap_usage: Math.round((usage.heapUsed / 1024 / 1024) * 100) / 100 + 'MB',
+    };
   }
 }
