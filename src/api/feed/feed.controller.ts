5dbcf14087bd2b036cc191426baa32ded651c00a
Alaa Zamel <alaa.zamel23@gmail.com>
2024-05-16 11:37:03 +0300

    feat: implement task (6) in the new list. get followed organizations' events

--- src/api/feed/feed.controller.ts
+++ src/api/feed/feed.controller.ts
@@ -1,10 +1,22 @@
-import { Controller, Get, Query } from '@nestjs/common';
+import { Controller, Get, Query, UseGuards } from '@nestjs/common';
 import { FeedService } from './feed.service';
 import { GenericFilter } from '../../common/interfaces/query.interface';
+import { AccessTokenGuard } from '../../auth/guards/access-token.guard';
+import { RoleGuard } from '../../common/guards/role/role.guard';
+import { Roles } from '../../common/decorators/roles.decorator';
+import { UserRoleEnum } from '../userRole/enums/user-role.enum';
+import { User } from '../../common/decorators/user.decorator';
+import { AuthUserType } from '../../common/types/auth-user.type';
+import { EventSerializer } from '../event/serializers/event.serializer';
+import { FileUtilityService } from '../../config/files/utility/file-utility.service';
+import { Event } from '../event/entities/event.entity';
 
 @Controller('feed')
 export class FeedController {
-  constructor(private readonly feedService: FeedService) {}
+  constructor(
+    private readonly feedService: FeedService,
+    private readonly fileUtilityService: FileUtilityService,
+  ) {}
 
   @Get('soonEvents')
   getSoonEvents(@Query() query: GenericFilter) {
@@ -19,5 +31,17 @@ export class FeedController {
   @Get('popularEvents')
   async getPopularEvents(@Query() query: GenericFilter) {
     return this.feedService.getPopularEvents(query);
+  }
+
+  @Get('followedEvents')
+  @Roles(UserRoleEnum.ATTENDEE)
+  @UseGuards(AccessTokenGuard, RoleGuard)
+  async getEventsOfFollowedOrganizations(
+    @User() user: AuthUserType,
+    @Query() query: GenericFilter,
+  ) {
+    const events: Event[] =
+      await this.feedService.getEventsOfFollowedOrganizations(user, query);
+    return EventSerializer.serializeList(this.fileUtilityService, events);
   }
 }
--- src/api/feed/feed.service.ts
+++ src/api/feed/feed.service.ts
@@ -1,14 +1,103 @@
 import { Injectable } from '@nestjs/common';
 import { Event } from '../event/entities/event.entity';
-import { DataSource } from 'typeorm';
+import { DataSource, In } from 'typeorm';
 import { GenericFilter } from '../../common/interfaces/query.interface';
 import { Organization } from '../organization/entities/organization.entity';
 import { AttendeeEvent } from '../attend-event/entities/attendee-event.entity';
 import { AttendeeEventStatus } from '../attend-event/enums/attendee-event-status.enum';
+import { AuthUserType } from '../../common/types/auth-user.type';
+import { Attendee } from '../attendee/entities/attendee.entity';
+import { FollowingAttendee } from '../organization/entities/following-attendee.entity';
+import { BlockedAttendee } from '../organization/entities/blocked-attendee.entity';
 
 @Injectable()
 export class FeedService {
   constructor(private readonly dataSource: DataSource) {}
+
+  async getEventsOfFollowedOrganizations(
+    user: AuthUserType,
+    query: GenericFilter,
+  ) {
+    // get the attendee id.
+    const attendeeId: number = await this.dataSource
+      .getRepository(Attendee)
+      .findOneBy({ user: { id: user.sub } })
+      .then((attendee) => attendee!.id);
+
+    // get the list of organization ids that are blocking the attendee.
+    const blockedOrganizationIds = await this.dataSource
+      .getRepository(BlockedAttendee)
+      .find({
+        where: { attendee: { id: attendeeId } },
+        relations: { organization: true },
+        select: { organization: { id: true } },
+      })
+      .then((entries) => {
+        return entries.map((entry) => {
+          return entry.organization.id;
+        });
+      });
+
+    // define the sub-query that can get the list
+    // of organization_ids that the attendee is following.
+    let followedOrganizationIdsQueryBuilder = this.dataSource
+      .getRepository(FollowingAttendee)
+      .createQueryBuilder('fa')
+      .where('attendee_id = :attendeeId', { attendeeId })
+      .innerJoin('fa.organization', 'o')
+      .select('o.id', 'id');
+
+    // exclude blocked organizations
+    if (blockedOrganizationIds.length > 0) {
+      followedOrganizationIdsQueryBuilder
+        .andWhere('o.id NOT IN (:...orgIds)')
+        .setParameters({ orgIds: blockedOrganizationIds });
+    }
+    // get the results
+    let followedOrganizationIds = await followedOrganizationIdsQueryBuilder
+      .getRawMany()
+      .then((entries) => {
+        return entries.map((entry) => {
+          return entry.id;
+        });
+      });
+
+    let events: Event[] = [];
+    if (followedOrganizationIds.length > 0) {
+      // Query the upcoming events for organizations
+      // that attendee is following & not being blocked by them.
+      let eventIds = await this.dataSource
+        .getRepository(Event)
+        .createQueryBuilder('e')
+        .where('e.organization_id IN (:...ids)')
+        .setParameters({ ids: followedOrganizationIds })
+        .leftJoin('e.days', 'ed')
+        .groupBy('e.id')
+        .addSelect('MIN(ed.dayDate)', 'start_day')
+        .orderBy('start_day', 'DESC')
+        .skip((query.page - 1) * query.pageSize)
+        .take(query.pageSize)
+        .addSelect('e.id', 'event_id')
+        .getRawMany()
+        .then((events) => events.map((ev) => +ev.event_id));
+
+      // slice the array of ids
+      eventIds = eventIds.slice(
+        (query.page - 1) * query.pageSize,
+        (query.page - 1) * query.pageSize + query.pageSize,
+      );
+
+      events = await this.dataSource.getRepository(Event).find({
+        where: {
+          id: In(eventIds),
+          organization: { id: In(followedOrganizationIds) },
+        },
+        order: { days: { dayDate: 'DESC' } },
+      });
+    }
+
+    return events;
+  }
 
   async getSoonEvents(query: GenericFilter) {
     const events = await this.dataSource
