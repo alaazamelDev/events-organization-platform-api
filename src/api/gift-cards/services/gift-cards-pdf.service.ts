9a8a789677898a32973d6920f07fb7f1f26827eb
HadiBa707 <73488175+HadiBa707@users.noreply.github.com>
2024-06-22 10:56:30 +0300

    print gift cards

--- src/api/gift-cards/services/gift-cards-pdf.service.ts
+++ src/api/gift-cards/services/gift-cards-pdf.service.ts
@@ -18,6 +18,9 @@ export class GiftCardsPdfService {
     });
     const page = await browser.newPage();
     await page.setViewport({ width: 1280, height: 720, deviceScaleFactor: 3 });
+    await page.setUserAgent(
+      'Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/95.0.4638.69 Safari/537.36',
+    );
 
     const htmlContent = `
       <html>
