753ecf43b2a5d016742a8a9c669a91d351d0f681
Alaa Zamel <alaa.zamel23@gmail.com>
2024-06-24 20:55:46 +0300

    feat: add some other types to the notification types seeder...

--- src/api/notification/entities/notification-type.entity.ts
+++ src/api/notification/entities/notification-type.entity.ts
@@ -3,6 +3,13 @@ import { Column, Entity } from 'typeorm';
 
 @Entity('notification_types')
 export class NotificationType extends BaseEntity {
+  // Define the static notification type ids...
+  static EVENT_CREATED: number = 1;
+  static ATTENDEE_ACCEPTED: number = 2;
+  static ATTENDEE_REJECTED: number = 3;
+  static GOT_REPLY: number = 4;
+  static GOT_REACTION: number = 5;
+
   @Column({
     type: 'varchar',
     name: 'name',
